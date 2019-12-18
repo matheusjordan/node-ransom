40f6db38b3d2d091cf71ecd7a4747fac9cc923af
matheusjordan <=>
2019-12-17 22:37:07 -0300

    created project pattern

---
+++ config/index.js
@@ -0,0 +1,6 @@
+const prodEnvironment = require('../environment/prod');
+const devEnvironment = require('../environment/dev');
+
+module.exports = process.env.NODE_ENV === 'production' ?
+                 prodEnvironment : devEnvironment;
+
